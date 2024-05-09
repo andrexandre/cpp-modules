9bcea312459cf76ab752302d02e452f2d9b4ab6b
andrexandre <106836082+andrexandre@users.noreply.github.com>
2024-05-09 17:59:04 +0100

    cpp8/ex00

--- cpp8/ex00/easyfind.hpp
+++ cpp8/ex00/easyfind.hpp
@@ -7,7 +7,12 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
-// template<typename T>
+template<typename T>
+typename T::iterator easyfind(T &container, int target)
+{
+	return std::find(container.begin(), container.end(), target);
+}
 
 #endif
--- cpp8/ex00/main.cpp
+++ cpp8/ex00/main.cpp
@@ -1,6 +1,6 @@
 #include "easyfind.hpp"
 
-int main(void)
+int main()
 {
 	return (0);
 }
